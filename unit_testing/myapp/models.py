4690fdc60e16af1338ebf233a55fcbae5e8c953a
Mehroze Zahid <mehroze.zahid@gmail.com>
2015-08-18 16:13:30 +0500

    Cache Unit Test

--- unit_testing/myapp/models.py
+++ unit_testing/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.cache import cache
 
 
 class Person(models.Model):
@@ -6,5 +7,27 @@ class Person(models.Model):
     first_name = models.TextField(max_length=50)
     last_name = models.TextField(max_length=50)
 
+    cache_key = u'person_{}'
+
     def __unicode__(self):
         return "{name}".format(name=self.first_name+self.last_name)
+
+    def save(self, *args, **kwargs):
+        super(Person, self).save(*args, **kwargs)
+        print("Object is saved")
+
+    def delete(self, *args, **kwargs):
+        cache.delete(self.first_name)
+        super(Person, self).delete(*args, **kwargs)
+
+    @classmethod
+    def get_person(self, fname):
+        cache_key = self.cache_key.format(fname)
+        val = cache.get(cache_key)
+
+        if val is None:
+            obj = Person.objects.get(first_name=fname)
+            cache.set(cache_key, obj)
+            return obj
+
+        return val
--- unit_testing/myapp/test_models.py
+++ unit_testing/myapp/test_models.py
@@ -1,15 +1,27 @@
+from django.core.cache import cache
 from django.test import TestCase
 from myapp.models import Person
 
 
 class PersonModelTest(TestCase):
-    def setUp(self):
-        Person.objects.create(first_name="Mehroze", last_name="Zahid")
-        Person.objects.create(first_name="Hello", last_name="World")
+    # def setUp(self):
+    #     Person.objects.create(first_name="Mehroze", last_name="Zahid")
+    #     Person.objects.create(first_name="Hello", last_name="World")
+    #
+    # def test_person_name(self):
+    #     person_1 = Person.objects.get(first_name="Mehroze")
+    #     person_2 = Person.objects.get(first_name="Hello")
+    #
+    #     self.assertEqual(person_1.last_name, "Zahid")
+    #     self.assertEqual(person_2.last_name, "World")
 
-    def test_person_name(self):
-        person_1 = Person.objects.get(first_name="Mehroze")
-        person_2 = Person.objects.get(first_name="Hello")
+    def test_cache(self):
 
-        self.assertEqual(person_1.last_name, "Zahid")
-        self.assertEqual(person_2.last_name, "World")
+        self.fname = 'Mehroze'
+        self.lname = 'Zahid'
+
+        Person.objects.create(first_name=self.fname, last_name=self.lname)
+
+        with self.assertNumQueries(1):
+            Person.get_person(self.fname)
+            Person.get_person(self.fname)
