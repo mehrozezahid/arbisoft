e6c21fdad086dfd14d1cc73161f64f1ea8783506
Mehroze Zahid <mehroze.zahid@gmail.com>
2015-08-27 14:39:42 +0500

    v2 - Completed

--- Image_Handling/Image_Handling/urls.py
+++ Image_Handling/Image_Handling/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from Image_Handling import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,4 +8,4 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^myapp/', include('myapp.urls', namespace='myapp')),
     url(r'^admin/', include(admin.site.urls)),
-)
+)  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Image_Handling/myapp/forms.py
+++ Image_Handling/myapp/forms.py
@@ -29,36 +29,36 @@ class UserLogin(forms.ModelForm):
 class UploadPictureForm(forms.Form):
     picture = forms.ImageField(label='Select an Image')
 
-    def save_image(self, user, pic):
-        """"Function saves image on disk in a path using default image
-         folder and separate directory for each user, and returns the
-         path of the image to be stored in db"""
-
-        # setting directory to IMAGE_PATH from settings + user directory
-        directory = settings.IMAGE_PATH + \
-                    user.username.encode('ascii', 'ignore') + '/'
-
-        # check if picture for user already exists, if so delete from
-        # db and disk
-        if UserProfile.objects.filter(user=user).exists():
-            shutil.rmtree(directory)
-            UserProfile.objects.filter(user=user).delete()
-
-        # check if directory exists, if not create
-        try:
-            os.makedirs(directory)
-        except OSError as exception:
-            if exception.errno != errno.EEXIST:
-                raise
-
-        # change filename to current date
-        directory += datetime.today().strftime('%d-%m-%Y')
-
-        # write file to disk
-        destination = open(directory, 'wb+')
-        for chunk in pic.chunks():
-            destination.write(chunk)
-            destination.close()
-
-        # return path to image
-        return directory
+    # def save_image(self, user, pic):
+    #     """"Function saves image on disk in a path using default image
+    #      folder and separate directory for each user, and returns the
+    #      path of the image to be stored in db"""
+    #
+    #     # setting directory to IMAGE_PATH from settings + user directory
+    #     directory = settings.IMAGE_PATH + \
+    #                 user.username.encode('ascii', 'ignore') + '/'
+    #
+    #     # check if picture for user already exists, if so delete from
+    #     # db and disk
+    #     if UserProfile.objects.filter(user=user).exists():
+    #         shutil.rmtree(directory)
+    #         UserProfile.objects.filter(user=user).delete()
+    #
+    #     # check if directory exists, if not create
+    #     try:
+    #         os.makedirs(directory)
+    #     except OSError as exception:
+    #         if exception.errno != errno.EEXIST:
+    #             raise
+    #
+    #     # change filename to current date
+    #     directory += datetime.today().strftime('%d-%m-%Y') + '.jpg'
+    #
+    #     # write file to disk
+    #     destination = open(directory, 'wb+')
+    #     for chunk in pic.chunks():
+    #         destination.write(chunk)
+    #         destination.close()
+    #
+    #     # return path to image
+    #     return directory
