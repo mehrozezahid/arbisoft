d4c3754e984d2d933941c89525b366dcb103b638
Mehroze Zahid <mehroze.zahid@gmail.com>
2015-09-01 16:50:02 +0500

    v3 - Authentication complete

--- Middlewares_ORM/Middlewares_ORM/backend/auth_backend.py
+++ Middlewares_ORM/Middlewares_ORM/backend/auth_backend.py
@@ -1,17 +1,25 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+import hashlib
 
 
 class AuthBackend(object):
 
-    def authenticate(self, email, password):
+    @staticmethod
+    def authenticate(email, password):
         print("Inside custom authenticate function with the user id being {username}".format(username=email))
         try:
             user = User.objects.get(email=email)
-            if user.password == password:
+            print("Inside try")
+            sha = hashlib.sha256(password)
+            print(user.password)
+            print(sha)
+            if user.check_password(password):
+                print("Returning user")
                 return user
 
         except ObjectDoesNotExist:
+            print("Returning None")
             return None
 
     def get_user(self, user_id):
