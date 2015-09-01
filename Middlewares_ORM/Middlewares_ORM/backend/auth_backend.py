4e3f577a5c5222d36476d29370ad823a382961c9
Mehroze Zahid <mehroze.zahid@gmail.com>
2015-09-01 16:55:21 +0500

    v4 - Refactoring

--- Middlewares_ORM/Middlewares_ORM/backend/auth_backend.py
+++ Middlewares_ORM/Middlewares_ORM/backend/auth_backend.py
@@ -7,19 +7,12 @@ class AuthBackend(object):
 
     @staticmethod
     def authenticate(email, password):
-        print("Inside custom authenticate function with the user id being {username}".format(username=email))
         try:
             user = User.objects.get(email=email)
-            print("Inside try")
-            sha = hashlib.sha256(password)
-            print(user.password)
-            print(sha)
             if user.check_password(password):
-                print("Returning user")
                 return user
 
         except ObjectDoesNotExist:
-            print("Returning None")
             return None
 
     def get_user(self, user_id):
